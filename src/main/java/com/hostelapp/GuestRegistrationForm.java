3348f364621473685d8ebe3e70af78d6e9d57915
ViniciusGomes <gomes.vinicius@sou.unifal-mg.edu.br>
2026-02-18 19:34:07 -0300

    implemented field editing in the guest creation process in GuestRegistrationForm class

--- src/main/java/com/hostelapp/GuestRegistrationForm.java
+++ src/main/java/com/hostelapp/GuestRegistrationForm.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class GuestRegistrationForm {
 
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
+    Guest guest = new Guest();
+    Address address = new Address();
 
     private void fillFirstName(Guest guest){
         while(true){
@@ -158,32 +160,132 @@ public class GuestRegistrationForm {
         }
     }
 
-    public static void main(String[] args) {
+    public Guest createGuest() {
 
-        Guest guest = new Guest();
-        Address address = new Address();
-        GuestRegistrationForm guestRegistration = new GuestRegistrationForm();
-
-        System.out.println("=====CADASTRO DE HOSPEDE=====\n");
-        guestRegistration.fillFirstName(guest);
-        guestRegistration.fillLastName(guest);
-        guestRegistration.fillTitle(guest);
-        guestRegistration.fillCPF(guest);
-        guestRegistration.fillBirthDate(guest);
-        guestRegistration.fillEMail(guest);
+        System.out.println("\n=====CADASTRO DE HOSPEDE=====\n");
+        fillFirstName(guest);
+        fillLastName(guest);
+        fillTitle(guest);
+        fillCPF(guest);
+        fillBirthDate(guest);
+        fillEMail(guest);
 
         System.out.println("\nINFORMACOES DE ENDERECO");
-        guestRegistration.fillZipCode(address);
-        guestRegistration.fillNumber(address);
-        guestRegistration.fillAddress(address);
-        guestRegistration.fillCity(address);
-        guestRegistration.fillState(address);
-        guestRegistration.fillCountry(address);
+        fillZipCode(address);
+        fillNumber(address);
+        fillAddress(address);
+        fillCity(address);
+        fillState(address);
+        fillCountry(address);
 
         guest.setAddress(address);
 
         System.out.println("Exibindo informacoes do hospede criado:");
         System.out.println(guest.toString());
 
+        int edit = edit();
+
+        while (edit == 2) {
+            System.out.println("Deseja editar informacoes do hospede ou da residencia dele?");
+            System.out.println("1- informacoes do hospede \n2- informacoes da residencia");
+            int guestOrAddress = scanner.nextInt();
+            scanner.nextLine();
+            if (guestOrAddress == 1){
+                editGuest();
+                edit = edit();
+                scanner.nextLine();
+            }else if (guestOrAddress == 2) {
+                editAddress();
+                edit = edit();
+                scanner.nextLine();
+            }
+        }
+
+        System.out.println("Informacoes salvas!");
+
+        return guest;
+
+    }
+
+    public int edit (){
+        System.out.println("\nDeseja salvar as informacoes do hospede?");
+        System.out.println("1- salvar" +
+                "\n2- editar");
+        int answer = scanner.nextInt();
+        while (answer != 1 && answer != 2){
+            System.out.println("opcao invalida, tente novamente.");
+            answer = scanner.nextInt();
+        }
+        return answer;
+    }
+
+    public void editGuest(){
+        System.out.println("EDITAR hospede:" +
+                "\n1- CPF \n2- titulo\n3- nome\n4- sobrenome" +
+                "\n5- data de nascimento\n6- email\n0- SAIR");
+
+        int option = scanner.nextInt();
+        scanner.nextLine();
+            switch (option) {
+                case 0:
+                    break;
+                case 1:
+                    fillCPF(guest);
+                    break;
+                case 2:
+                    fillTitle(guest);
+                    break;
+                case 3:
+                    fillFirstName(guest);
+                    break;
+                case 4:
+                    fillLastName(guest);
+                    break;
+                case 5:
+                    fillBirthDate(guest);
+                    break;
+                case 6:
+                    fillEMail(guest);
+                    break;
+                default:
+                    System.out.println("Opcao invalida digitada. Tente novamente.");
+                    option = scanner.nextInt();
+            }
+        System.out.println("saindo do modo de edicao.\n");
+        System.out.println(guest.toString());
+    }
+
+    public void editAddress (){
+        System.out.println("EDITAR endereco:" +
+                "\n1- numero\n2- rua\n3- CEP\n4- cidade\n5- estado\n6- pais\n0- SAIR");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+            switch (option) {
+                case 0:
+                    break;
+                case 1:
+                    fillNumber(address);
+                    break;
+                case 2:
+                    fillAddress(address);
+                    break;
+                case 3:
+                    fillZipCode(address);
+                    break;
+                case 4:
+                    fillCity(address);
+                    break;
+                case 5:
+                    fillState(address);
+                    break;
+                case 6:
+                    fillCountry(address);
+                    break;
+                default:
+                    System.out.println("Opcao invalida digitada. Tente novamente.");
+                    option = scanner.nextInt();
+            }
+        System.out.println("saindo do modo de edicao.\n");
+        System.out.println(guest.toString());
     }
 }
--- src/main/java/com/hostelapp/GuestTest.java
+++ src/main/java/com/hostelapp/GuestTest.java
@@ -5,19 +5,7 @@ import java.util.Scanner;
 
 public class GuestTest {
     public static void main(String[] args) {
-
-//        Guest guest = new Guest();
-//        Scanner scanner = new Scanner(System.in);
-
-//        System.out.println("Informe o nome do hóspede:");
-//        guest.setFirstName(scanner.nextLine());
-//        System.out.println("Informe o último sobrenome do hóspede:");
-//        guest.setLastName(scanner.nextLine());
-
-//        Guest guest = new Guest("Florentino", "Ariza", "florentinoariza@gmail.com");
-//        Address address = new Address("669", "Boaventura",
-//                "37133800", "Belo Horizonte", "MG", "Brasil");
-//        guest.setAddress(address);
-//        System.out.println(guest.toString());
+        GuestRegistrationForm guestRegister = new GuestRegistrationForm();
+        Guest guest = guestRegister.createGuest();
     }
 }
